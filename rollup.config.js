49a9c24f26c73bd8d64977dfbed6a97643f59ebd
shaolonger <shaolong.chuang@qq.com>
2020-09-17 21:51:29 +0800

    build: 新增rollup-plugin-terser插件，压缩打包后的js文件

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,22 @@
 // rollup.config.js
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
+import {
+    terser
+} from 'rollup-plugin-terser';
 
 export default {
     input: 'src/index.js',
-    output: {
+    output: [{
         file: 'dist/log4browser.js',
         format: 'umd',
         name: 'Log4Browser'
-    },
+    }, {
+        file: 'dist/log4browser.min.js',
+        format: 'umd',
+        name: 'Log4Browser',
+        plugins: [terser()]
+    }],
     plugins: [
         resolve(),
         babel({
