28e01bf53d7cb20df5ca38c17b34b625653d52c1
shaolonger <shaolong.chuang@qq.com>
2020-09-17 16:43:13 +0800

    build(rollup):更新配置文件， 引入rollup的babel插件

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,18 @@
 // rollup.config.js
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+
 export default {
     input: 'src/index.js',
     output: {
         file: 'dist/log4browser.js',
         format: 'umd',
         name: 'Log4Browser'
-    }
+    },
+    plugins: [
+        resolve(),
+        babel({
+            babelHelpers: 'bundled'
+        })
+    ]
 };
